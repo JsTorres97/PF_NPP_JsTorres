46c4bab9841c1950d565854ac33e1da4e0f52ec6
Jesús Torres <stbazany@gmail.com>
2018-11-21 18:26:54 -0600

    funciona el módulo de la fecha y hora, además de que la clase de conexion tiene implementado la función de buscar al cliente, pero no funciona la conexion

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        fecha()
+        lbl_cliente.Text = ""
+
+
+
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lbl_hora.Text = String.Format("{0:HH:mm:ss}", DateTime.Now)
+        Timer1.Start()
+    End Sub
+
+    Private Sub fecha()
+        lbl_hora.Text = String.Format("{0:HH:mm:ss}", DateTime.Now)
+        Dim meses = DateTime.Now.ToString("MM")
+        Dim diaSemana = Format(Now, "dddd")
+        Dim diaMes = Format(Now, "dd")
+        Dim mes = Thread.CurrentThread.CurrentCulture.DateTimeFormat.MonthNames(meses - 1)
+        Dim año = Format(Now, "yyyy")
+        lbl_fecha.Text = diaSemana + ", " + diaMes + " de " + mes + " del " + año
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim id = txt_id.Text
+        Dim buscar As New conexiones
+        Dim nombre = buscar.BuscarCliente(id)
+        If nombre = "" Then
+            MessageBox.Show("No existe cliente con ese número, favor de buscar de nuevo", "Cliente no encontrado",
+            MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk)
+            lbl_cliente.Text = nombre
+        Else
+            lbl_cliente.Text = nombre
+        End If
+    End Sub
+End Class
---
+++ WindowsApp1/conexiones.vb
@@ -0,0 +1,111 @@
+﻿Imports Npgsql
+
+Public Class conexiones
+    Dim strConexion As String = "Server=127.0.0.1;port=5432;Database=a00260271;uid=a00260271;pwd=a00260271"
+
+    Public Function ExisteUsuario(ByVal strNombre As String, ByVal strApellido As String) As Boolean
+        Dim conn As New NpgsqlConnection(strConexion)
+        Dim respuesta As Boolean
+        Try
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+                Dim query As String = "SELECT * FROM usuarios WHERE nombre='" + strNombre + "' AND apellidos='" + strApellido + "'"
+                Dim comando As NpgsqlCommand = New NpgsqlCommand(query, conn)
+                Dim da As NpgsqlDataReader
+                da = comando.ExecuteReader
+                If da.Read Then
+
+
+                    respuesta = True
+                Else
+                    respuesta = False
+                End If
+                conn.Close()
+            End If
+        Catch ex As Exception
+            MsgBox("Error: " + Err.ToString)
+
+        End Try
+        Return respuesta
+
+    End Function
+
+    Public Function BuscarCliente(ByVal IdCliente As String) As String
+        Dim conn As New NpgsqlConnection(strConexion)
+        Dim nombre As String
+        nombre = ""
+        Try
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+                Dim query As String = "SELECT NOMBRE FROM CLIENTES WHERE ID='" + IdCliente + "'"
+                Dim comando As NpgsqlCommand = New NpgsqlCommand(query, conn)
+                Dim da As NpgsqlDataReader
+                da = comando.ExecuteReader
+                If da.Read Then
+                    nombre = da.Item("nombre").ToString
+                Else
+                    nombre = ""
+                End If
+            End If
+            conn.Close()
+        Catch ex As Exception
+            MsgBox("Error: " + Err.ToString)
+        End Try
+        Return nombre
+    End Function
+
+
+    'Public Function UsuarioValido(ByVal strUsuario As String, ByVal strPassword As String) As Boolean
+    '    Dim conn As New NpgsqlConnection(strConexion)
+    '    Dim respuesta As Boolean
+    '    Try
+    '        If conn.State = ConnectionState.Closed Then
+    '            conn.Open()
+    '            Dim query As String = "SELECT id, clientes, provedores, productos, ventas, compras, reportes, usuarios FROM usuarios WHERE nombre='" + strUsuario + "' AND contrasena='" + strPassword + "'"
+    '            Dim comando As NpgsqlCommand = New NpgsqlCommand(query, conn)
+    '            Dim da As NpgsqlDataReader
+    '            da = comando.ExecuteReader
+    '            If da.Read Then
+    '                gClientes = da.Item("clientes").ToString
+    '                gProveedores = da.Item("provedores").ToString
+    '                gProductos = da.Item("productos").ToString
+    '                gVentas = da.Item("ventas").ToString
+    '                gCompras = da.Item("compras").ToString
+    '                gReportes = da.Item("reportes").ToString
+    '                gUsuarios = da.Item("usuarios").ToString
+
+
+    '                respuesta = True
+    '            Else
+    '                respuesta = False
+    '            End If
+    '            conn.Close()
+    '        End If
+    '    Catch ex As Exception
+    '        MsgBox("Error: " + Err.ToString)
+
+    '    End Try
+    '    Return respuesta
+    'End Function
+
+
+    Public Function EjecutaQuery(ByVal Query As String) As Boolean
+        Dim conn As New NpgsqlConnection(strConexion)
+        Dim respuesta As Boolean
+        Try
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+                Dim comando As NpgsqlCommand = New NpgsqlCommand(Query, conn)
+                comando.ExecuteNonQuery()
+                respuesta = True
+                conn.Close()
+            End If
+        Catch ex As Exception
+            MsgBox("Error: " + Err.ToString)
+
+        End Try
+        Return respuesta
+
+    End Function
+
+End Class
